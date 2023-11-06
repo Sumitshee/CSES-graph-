4c0e0a5f0a2e9e1bb0f8744b857caa5cc8c0efe4
Sumitshee <89238652+Sumitshee@users.noreply.github.com>
2023-11-06 11:32:14 +0530

    Create Counting_Rooms.cpp
    https://cses.fi/problemset/task/1192

---
+++ Counting_Rooms.cpp
@@ -0,0 +1,90 @@
+/*/ Author: Sumit8707 /*/
+ 
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h> 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define int long long int
+#define endl "\n"
+#define ff first
+#define ss second
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
+#define min3(a, b, c) min(min((a), (b)), (c))
+#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+ 
+ 
+template<class T> using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<class T> using moset = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+void kick_start(int t) {cout << "Case #" << t << ": ";}
+ 
+void dfs(vector<vector<char>>&grid, vector<vector<bool>>&visited,int i, int j, int n,int m){
+    visited[i][j]=true;
+    int dy[] = {0, 0, 1, -1};
+    int dx[] = {1, -1, 0, 0};
+    for(int k = 0; k<4; k++){
+        int nx = i + dx[k];
+        int ny = j + dy[k];
+        if(nx>=0 and ny>=0 and nx<n and ny<m and grid[nx][ny]=='.' and !visited[nx][ny]){
+            dfs(grid,visited,nx,ny,n,m);
+        }
+    }
+}
+ 
+/*------------------------------------------------------------------------------------------------------------*/
+void wrong_submission(){
+    int n,m; cin>>n>>m;
+    vector<vector<char>>grid(n,vector<char>(m,'#'));
+    vector<vector<bool>>visited(n,vector<bool>(m,false));
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j<m; j++){
+            cin>>grid[i][j];
+        }
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j<m; j++){
+            if(!visited[i][j] and grid[i][j]=='.'){
+                dfs(grid,visited,i,j,n,m);
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+}
+/*------------------------------------------------------------------------------------------------------------*/ 
+int32_t main() {
+   ios_base::sync_with_stdio(0); 
+   cin.tie(0); cout.tie(0);
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+   freopen("output.txt","w",stdout);
+   #endif
+ 
+   auto start = std::chrono::high_resolution_clock::now();
+ 
+     int t=1;
+     // cin>> t;
+      for(int i = 1; i <= t; i++){
+           // kick_start(i);
+           wrong_submission();
+      }
+ 
+ 
+   auto stop = std::chrono::high_resolution_clock::now(); 
+   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+ 
+   // #ifndef ONLINE_JUDGE
+   // cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"s "<< endl;    
+   // #endif
+   return 0;
+}
